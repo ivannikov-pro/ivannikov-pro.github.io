021c5cf0386d6e05d21d2bdf924b9a17f1f1d14d
an-ivannikov <a.n.ivannikov@protonmail.com>
2026-03-29 14:36:25 +0400

    Updates

---
+++ _next/static/C6oBR5Wx9ptB2D7d5DB7T/_clientMiddlewareManifest.js
@@ -0,0 +1,10 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/(\\/?index|\\/?index\\.json))?[\\/#\\?]?$",
+    "originalSource": "/"
+  },
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next|_vercel|.*\\..*).*))(\\.json)?[\\/#\\?]?$",
+    "originalSource": "/((?!_next|_vercel|.*\\..*).*)"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
