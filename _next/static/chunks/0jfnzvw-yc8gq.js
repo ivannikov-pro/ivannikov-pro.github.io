1183888fffe634aa9847e5da72b498c27f956976
Aleksandr Ivannikov <hi@ivannikov.pro>
2026-05-23 19:53:55 +0400

    deploy: refresh from ivannikov-pro-platform

---
+++ _next/static/chunks/0jfnzvw-yc8gq.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,996663,o=>{"use strict";var r=o.i(583979),t=o.i(820856);o.s(["default",0,function({locale:o,...e}){if(!o)throw Error(void 0);return(0,t.jsx)(r.IntlProvider,{locale:o,...e})}])}]);
